16b39df484782e6c51e41d0296c781c1b8022073
dharmendra <dharmendra.soni@scaletech.xyz>
2024-01-11 15:37:54 +0530

    installed basic routes

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-console.log("hello")
+import { app } from "./app";
+import { APPLICATION_PORT } from "./config";
+import { logger } from "./utils/logger";
+
+app.listen(APPLICATION_PORT, '0.0.0.0', () => {
+    return logger.info(`application started for the port ${APPLICATION_PORT} visit = http://0.0.0.0:${APPLICATION_PORT}`);
+})
